a19d12ff05bad9e9684b10f17cd9a5738429a0b1
USER <anakhabiji2000@gmail.com>
2022-11-21 19:50:32 +0530

    commit

--- bin/classdemo.dart
+++ bin/classdemo.dart
@@ -1,17 +1,28 @@
 class StudentData{
-  String name='Anakha' ;
-  int  age=22;
-  int number=8129505419;
+  String? name;
+  int?  age;
+  int? number;
  static String college="SMC";
 }
  void main(){
 
    StudentData stu1= StudentData();
+   StudentData stu2= StudentData();
+   StudentData stu3= StudentData();
    print("STUDENT_DETAILS");
-   print("name:${stu1.name}");
-   print("age:${stu1.age}");
-   print("number:${stu1.number}");
-   print("college: ${StudentData.college}");
+   print("Name:${stu1.name= 'Anakha'}");
+   print("Age:${stu1.age=22}");
+   print("Number:${stu1.number=8129505419}");
+   print("College: ${StudentData.college}");
 
 
+   print("Name:${stu2.name= ' Anju'}");
+   print("Age:${stu2.age= 21}");
+   print("Number:${stu2.number=9128505509}");
+   print("College: ${StudentData.college}");
+
+   print("Name:${stu3.name= 'Shibina'}");
+   print("Age:${stu2.age= 22}");
+   print("Number:${stu2.number=7829906505}");
+   print("College: ${StudentData.college}");
  }
